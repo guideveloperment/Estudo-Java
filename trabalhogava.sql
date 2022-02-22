481cc13ff3e031579f39ca2c01ad3dc43a4087cd
Guilherme <vianaguilherme29@gmail.com>
2022-02-22 01:37:05 -0300

    alteracoes pag pedido

---
+++ trabalhogava.sql
@@ -0,0 +1,12 @@
+create table gavatrabalho(
+	id int(3) not null auto_increment,
+	nome varchar(120) not null,
+	country varchar(120),
+	email varchar(220) not null,
+	primary key (id)
+
+);
+
+drop table GavaTrabalho;
+show tables;
+select * from GavaTrabalho;
